94bec956a846fc6527fa27ff100c8936c1c6dc4a
salma <okba.salma24@gmail.com>
2024-10-27 23:17:46 +0100

    question 6 et 5 faite

--- applicationtest/urls.py
+++ applicationtest/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import about, index  # Importation correcte
+from .views import about, index, collec_detail,collection_list  # Importation correcte
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('collection/<int:id>/', collec_detail, name='collec_detail'),
+    path('all/', collection_list, name='collection_list'),
 ]
--- applicationtest/views.py
+++ applicationtest/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render  # Importation en haut
-
+from django.shortcuts import render, get_object_or_404  # Importation en haut
+from .models import Collec
 # Création des vues
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def collec_detail(request, id):
+    collection = get_object_or_404(Collec, id=id)
+    return render(request, 'collec_detail.html', {'collection': collection})
+
+def collection_list(request):
+    collections = Collec.objects.all()
+    return render(request, 'collection_list.html', {'collections': collections})
